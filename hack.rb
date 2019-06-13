56ec21828f95b17bf664d8d8d73e406aad0d7f28
Michael Schwartz <MichaelSchwar3@gmail.com>
2019-06-13 11:48:24 -0400

    Update algorithm flexibility

--- hack.rb
+++ hack.rb
@@ -1,7 +1,19 @@
 def hackify(string)
-    string.gsub("e", "3")
+    string.split("").map { |ch| hack_char(ch) }.join("")
+    # string.gsub("e", "3")
 end
 
-def hackify(string)
-    string.gsub("a", "4")
+def hack_char(char)
+    case char:
+    when "e"
+        return "3"
+    when "i"
+        return "1"
+    when "a"
+        return "4"
+    when "o"
+        return "0"
+    else
+        return char
+    end
 end
